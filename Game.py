38e41e055be25fb99755aab2003c53b737c78c33
Maxrobloxian <134041250+Maxrobloxian@users.noreply.github.com>
2024-04-10 12:29:39 +0200

    Add files via upload

--- Game.py
+++ Game.py
@@ -1,37 +1,56 @@
-import pygame
-import time
- 
+import pygame, time, math
+
 pygame.init()
- 
+
 clock = pygame.time.Clock()
 autog = 0
 coins = 0
- 
+
+circles = []
+squares = []
+
 gameDisplay = pygame.display.set_mode((800, 600))
 pygame.display.set_caption("clicky clicks")
- 
-def circle(display, color, x, y, radius):
-    pygame.draw.circle(display, color, [x, y], radius)
- 
+
+class Circle:
+    def __init__(self, color, position, radius):
+        self.color = color
+        self.position = position
+        self.radius = radius
+
+    def draw(self):
+        pygame.draw.circle(gameDisplay, self.color, self.position, self.radius)
+
+class Square:
+    def __init__(self, color, position, size):
+        self.color = color
+        self.position = position
+        self.size = size
+
+    def draw(self):
+        pygame.draw.rect(gameDisplay, self.color, (self.position[0], self.position[1], self.size[0], self.size[1]))
+
 def autominer():
     global coins
     global autog
     time.sleep(0.1)
-    coins = coins + autog
- 
- 
+    coins += autog
+
+
 def DrawText(text, Textcolor, x, y, fsize):
     font = pygame.font.Font('freesansbold.ttf', fsize)
     text = font.render(text, True, Textcolor)
     textRect = text.get_rect()
     textRect.center = (x, y)
     gameDisplay.blit(text, textRect)
- 
- 
-def rectangle(display, color, x, y, w, h):
-    pygame.draw.rect(display, color, (x, y, w, h))
- 
- 
+
+circles.append(Circle((108, 77, 59), (400, 260), 160))
+
+squares.append(Square((0, 100, 250), (50, 500), (200, 50)))
+squares.append(Square((0, 155, 255), (55, 505), (190, 40)))
+squares.append(Square((0, 100, 250), (550, 500), (200, 50)))
+squares.append(Square((0, 155, 255), (555, 505), (190, 40)))
+
 def main_loop():
     global clock
     global autog
@@ -41,51 +60,53 @@ def main_loop():
     mong = 1
     cost = 50
     cost2 = 50
+    lvl = 0
+    lvl2 = 0
     global coins
+    coins = 5000
     while True:
         autominer()
         for event in pygame.event.get():
             if event.type == pygame.QUIT:
                 return False
- 
+
             if event.type == pygame.MOUSEBUTTONDOWN:
                 mPos = pygame.mouse.get_pos()
-                if mPos >= (350, 0):
-                    if mPos <= (450, 0):
-                        coins += mong
- 
-                if mPos <= (800, 0):
-                    if mPos >= (600, 0):
-                        if coins >= cost:
-                            coins = coins - cost
-                            cost = cost * 1.5
-                            mong = mong * 1.1
-                            cost = round(cost, 0)
- 
-                if mPos >= (50, 0):
-                    if mPos <= (245, 0):
-                        if coins >= cost2:
-                            coins = coins - cost2
-                            cost2 = cost2 * 1.5
-                            autog = autog + 0.5
-                            cost2 = round(cost2, 0)
- 
-                if coins == 2147483647:
-                    print("You Beat the game")
+                if (math.sqrt((mPos[0] - 400) ** 2 + (mPos[1] - 260) ** 2)) <= 160:
+                    coins += mong
+
+                if (mPos[0] >= 50 and mPos[1] >= 500) and (mPos[0] <= 250 and mPos[1] <= 550) and coins >= cost:
+                    coins -= cost
+                    cost *= 1.5
+                    cost = round(cost, 0)
+                    mong *= 1.1
+                    lvl += 1
+
+                if (mPos[0] >= 550 and mPos[1] >= 500) and (mPos[0] <= 750 and mPos[1] <= 550) and coins >= cost2:
+                    coins -= cost2
+                    cost2 *= 1.5
+                    cost2 = round(cost2, 0)
+                    autog += 0.5
+                    lvl2 += 1
+
+            if event.type == pygame.KEYDOWN:
+                if event.key == pygame.K_ESCAPE:
                     return False
- 
+
         gameDisplay.fill((173, 216, 230))
+
+        for i in circles: i.draw()
+        for i in squares: i.draw()
+
         DrawText(str(f'{coins:.2f}') + " coins", (0, 0, 0), 400, 50, 20)
-        DrawText("upgrade clicker " + str(cost), (0, 0, 0), 650, 480, 20)
-        DrawText("buy auto miner " + str(cost2), (0, 0, 0), 150, 480, 20)
-        rectangle(gameDisplay, (0, 100, 250), 50, 500, 200, 50)
-        rectangle(gameDisplay, (0, 155, 255), 55, 505, 190, 40)
-        circle(gameDisplay, (108, 77, 59), 400, 260, 160)
-        rectangle(gameDisplay, (0, 100, 250), 550, 500, 200, 50)
-        rectangle(gameDisplay, (0, 155, 255), 555, 505, 190, 40)
+        DrawText("upgrade clicker " + str(lvl), (0, 0, 0), 150, 480, 20)
+        DrawText(str(cost), (0, 0, 0), 150, 525, 20)
+        DrawText("buy auto miner " + str(lvl2), (0, 0, 0), 650, 480, 20)
+        DrawText(str(cost2), (0, 0, 0), 650, 525, 20)
+
         pygame.display.update()
         clock.tick(60)
- 
+
 main_loop()
 pygame.quit()
 quit()
